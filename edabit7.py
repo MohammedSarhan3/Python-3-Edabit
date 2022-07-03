c5eca345dc9fbeb9cd9a93217927724e4a081649
Mohammad Wael Sarhan <saraheeeen@gmail.com>
2022-07-03 12:22:39 +0200

    Solving problem 7

---
+++ edabit7.py
@@ -0,0 +1,3 @@
+#Fix the code in the code tab to pass this challenge (only syntax errors). Look at the examples below to get an idea of what the function should do.
+def squared(b):
+    return b * b
