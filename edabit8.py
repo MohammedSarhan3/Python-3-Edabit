0cf4f17d4efea9b3064a4662479f7c78396051bc
Mohammad Wael Sarhan <saraheeeen@gmail.com>
2022-07-03 12:23:10 +0200

    Solving problem 8

---
+++ edabit8.py
@@ -0,0 +1,5 @@
+#Create a function that finds the maximum range of a triangle's third edge, where the side lengths are all integers.
+
+def next_edge(side1, side2):
+	    range=(side1 + side2) - 1
+	    return range
