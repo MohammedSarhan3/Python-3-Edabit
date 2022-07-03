488b28a738b6dc82694e591c862a781dd0213270
Mohammad Wael Sarhan <saraheeeen@gmail.com>
2022-07-03 12:24:41 +0200

    Solving problem 10

---
+++ edabit10.py
@@ -0,0 +1,10 @@
+'''
+There is a single operator in Python, capable of providing the remainder
+of a division operation. Two numbers are passed as parameters.
+The first parameter divided by the second parameter will have a remainder,
+possibly zero. Return that value.
+
+
+'''
+def remainder(x, y):
+	return x%y
